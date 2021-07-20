063497ca1694f002fa15be47a9f823ecf60f56e0
Velizar Ivanov <zivanof@gmail.com>
2021-07-20 09:37:12 +0300

    Changed backend endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'https://app.vinteres.io/api/',
-  ws_url: 'wss://app.vinteres.io',
+  api_url: 'https://api.vinteres.io/api/',
+  ws_url: 'wss://api.vinteres.io',
   terms_and_usage_url: 'https://vinteres.io/terms-of-usage',
   privacy_policy_url: 'https://vinteres.io/privacy-policy',
 };
